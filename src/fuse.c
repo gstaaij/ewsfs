3decb3dce12e3dea11c21806c48f8b5a4dedddd8
gstaaij <gstaaij@protonmail.com>
2024-05-22 14:49:42 +0200

    Add `ewsfs.txt` hello world file
    Co-authored-by: JMBrands <JMBrands@users.noreply.github.com>
    Co-authored-by: Raymanroy <Raymanroy@users.noreply.github.com>

--- src/fuse.c
+++ src/fuse.c
@@ -6,7 +6,55 @@
 
 // Example from https://wiki.osdev.org/FUSE
 
-static struct fuse_operations ewsfsOps = {
+static int ewsfs_getattr(const char* path, struct stat* st) {
+    if (strcmp(path, "/") == 0) {
+        // It's the root directory
+        st->st_mode = S_IFDIR | 0755;
+        st->st_nlink = 2;
+        st->st_size = 4096;
+    } else if (strcmp(path, "/ewsfs.txt") == 0) {
+        // It's the `ewsfs.txt` file
+        st->st_mode = S_IFREG | 0644;
+        st->st_nlink = 2;
+        st->st_size = 13;
+    } else {
+        st->st_mode = S_IFREG | 0644;
+        st->st_nlink = 2;
+        st->st_size = 4096;
+    }
+    // User and group. we use the user's id who is executing the FUSE driver
+    st->st_uid = getuid();
+    st->st_gid = getgid();
+    return 0;
+}
+
+static int ewsfs_readdir(const char* path, void* buffer, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info* fi) {
+    (void) path;
+    (void) offset;
+    (void) fi;
+    filler(buffer, ".", NULL, 0);
+    filler(buffer, "..", NULL, 0);
+    filler(buffer, "ewsfs.txt", NULL, 0);
+    return 0;
+}
+
+static int ewsfs_read(const char* path, char* buffer, size_t size, off_t offset, struct fuse_file_info* fi) {
+    (void) offset;
+    (void) fi;
+    if (strcmp(path, "/ewsfs.txt") == 0) {
+        const char* text = "Hello, World!";
+        for (size_t i = 0; i < size && i < 13; ++i) {
+            buffer[i] = text[i];
+        }
+        return 13;
+    }
+    return -1;
+}
+
+static struct fuse_operations ewsfs_ops = {
+    .getattr = ewsfs_getattr,
+    .readdir = ewsfs_readdir,
+    .read = ewsfs_read,
 };
 
 char* devfile = NULL;
@@ -23,5 +71,5 @@ int main(int argc, char** argv) {
     }
 
     // leave the rest to FUSE
-    return fuse_main(argc, argv, &ewsfsOps, NULL);
+    return fuse_main(argc, argv, &ewsfs_ops, NULL);
 }
