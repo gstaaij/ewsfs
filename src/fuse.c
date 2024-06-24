0f9b9f252022e4caae3666055ad8b6f0db49997d
gstaaij <gstaaij@protonmail.com>
2024-06-24 13:00:42 +0200

    Add the ability to write to `ewsfs.txt`
    It's a temporary solution to see how the FUSE functions for writing work
    It currently just saves the file contents in memory

--- src/fuse.c
+++ src/fuse.c
@@ -4,6 +4,9 @@
 #include <unistd.h>
 #include <string.h>
 
+#define HELLOWORLD_COUNT 128
+static char ewsfs_filecontents[HELLOWORLD_COUNT];
+
 // Example from https://wiki.osdev.org/FUSE
 
 static int ewsfs_getattr(const char* path, struct stat* st) {
@@ -16,7 +19,9 @@ static int ewsfs_getattr(const char* path, struct stat* st) {
         // It's the `ewsfs.txt` file
         st->st_mode = S_IFREG | 0644;
         st->st_nlink = 2;
-        st->st_size = 13;
+        st->st_size = strlen(ewsfs_filecontents);
+        if (st->st_size > HELLOWORLD_COUNT)
+            st->st_size = HELLOWORLD_COUNT;
     } else {
         st->st_mode = S_IFREG | 0644;
         st->st_nlink = 2;
@@ -42,11 +47,43 @@ static int ewsfs_read(const char* path, char* buffer, size_t size, off_t offset,
     (void) offset;
     (void) fi;
     if (strcmp(path, "/ewsfs.txt") == 0) {
-        const char* text = "Hello, World!";
-        for (size_t i = 0; i < size && i < 13; ++i) {
-            buffer[i] = text[i];
+        size_t bytecount = 0;
+        for (size_t i = 0; i < size && i < HELLOWORLD_COUNT && ewsfs_filecontents[i] != '\0'; ++i) {
+            buffer[i] = ewsfs_filecontents[i];
+            bytecount++;
         }
-        return 13;
+        return bytecount;
+    }
+    return -1;
+}
+
+static int ewsfs_open(const char* path, struct fuse_file_info* fi) {
+    (void) fi;
+    if (strcmp(path, "/ewsfs.txt") == 0) {
+        return 0;
+    }
+    return -1;
+}
+
+static int ewsfs_truncate(const char* path, off_t offset) {
+    (void) offset;
+    if (strcmp(path, "/ewsfs.txt") == 0) {
+        return 0;
+    }
+    return -1;
+}
+
+static int ewsfs_write(const char* path, const char* buffer, size_t size, off_t offset, struct fuse_file_info* fi) {
+    (void) offset;
+    (void) fi;
+    if (strcmp(path, "/ewsfs.txt") == 0) {
+        size_t bytecount = 0;
+        for (size_t i = 0; i < size && i < HELLOWORLD_COUNT; ++i) {
+            ewsfs_filecontents[i] = buffer[i];
+            bytecount++;
+        }
+        // For now, we return size, so text larger than 128 bytes gets properly truncated and doesn't overwrite itself
+        return size;
     }
     return -1;
 }
@@ -55,6 +92,9 @@ static struct fuse_operations ewsfs_ops = {
     .getattr = ewsfs_getattr,
     .readdir = ewsfs_readdir,
     .read = ewsfs_read,
+    .open = ewsfs_open,
+    .truncate = ewsfs_truncate,
+    .write = ewsfs_write,
 };
 
 char* devfile = NULL;
