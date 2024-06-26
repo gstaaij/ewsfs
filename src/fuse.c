55ff6dda0a2233a4048b279ebd668d9a81ba9302
gstaaij <gstaaij@protonmail.com>
2024-06-26 11:59:42 +0200

    Make the filename of the hello world file a macro

--- src/fuse.c
+++ src/fuse.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <string.h>
 
+#define HELLOWORLD_FILE "ewsfs.txt"
 #define HELLOWORLD_COUNT 128
 static char ewsfs_filecontents[HELLOWORLD_COUNT];
 
@@ -15,7 +16,7 @@ static int ewsfs_getattr(const char* path, struct stat* st) {
         st->st_mode = S_IFDIR | 0755;
         st->st_nlink = 2;
         st->st_size = 4096;
-    } else if (strcmp(path, "/ewsfs.txt") == 0) {
+    } else if (strcmp(path, "/"HELLOWORLD_FILE) == 0) {
         // It's the `ewsfs.txt` file
         st->st_mode = S_IFREG | 0644;
         st->st_nlink = 2;
@@ -39,14 +40,14 @@ static int ewsfs_readdir(const char* path, void* buffer, fuse_fill_dir_t filler,
     (void) fi;
     filler(buffer, ".", NULL, 0);
     filler(buffer, "..", NULL, 0);
-    filler(buffer, "ewsfs.txt", NULL, 0);
+    filler(buffer, HELLOWORLD_FILE, NULL, 0);
     return 0;
 }
 
 static int ewsfs_read(const char* path, char* buffer, size_t size, off_t offset, struct fuse_file_info* fi) {
     (void) offset;
     (void) fi;
-    if (strcmp(path, "/ewsfs.txt") == 0) {
+    if (strcmp(path, "/"HELLOWORLD_FILE) == 0) {
         size_t bytecount = 0;
         for (size_t i = 0; i < size && i < HELLOWORLD_COUNT && ewsfs_filecontents[i] != '\0'; ++i) {
             buffer[i] = ewsfs_filecontents[i];
@@ -59,7 +60,7 @@ static int ewsfs_read(const char* path, char* buffer, size_t size, off_t offset,
 
 static int ewsfs_open(const char* path, struct fuse_file_info* fi) {
     (void) fi;
-    if (strcmp(path, "/ewsfs.txt") == 0) {
+    if (strcmp(path, "/"HELLOWORLD_FILE) == 0) {
         return 0;
     }
     return -1;
@@ -67,7 +68,7 @@ static int ewsfs_open(const char* path, struct fuse_file_info* fi) {
 
 static int ewsfs_truncate(const char* path, off_t offset) {
     (void) offset;
-    if (strcmp(path, "/ewsfs.txt") == 0) {
+    if (strcmp(path, "/"HELLOWORLD_FILE) == 0) {
         return 0;
     }
     return -1;
@@ -76,7 +77,7 @@ static int ewsfs_truncate(const char* path, off_t offset) {
 static int ewsfs_write(const char* path, const char* buffer, size_t size, off_t offset, struct fuse_file_info* fi) {
     (void) offset;
     (void) fi;
-    if (strcmp(path, "/ewsfs.txt") == 0) {
+    if (strcmp(path, "/"HELLOWORLD_FILE) == 0) {
         size_t bytecount = 0;
         for (size_t i = 0; i < size && i < HELLOWORLD_COUNT; ++i) {
             ewsfs_filecontents[i] = buffer[i];
